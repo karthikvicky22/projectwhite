98eb151955aceb0818cc197c23886a13ad607142
karthikvicky22 <karthiksharon003@gmail.com>
2023-03-02 15:45:59 +0530

    code added

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -8,7 +8,9 @@ System.out.println("karthi");
 private void phoneNumb() {
 System.out.println("9629087326");
 }
-
+private void osName() {
+	System.out.println("android");
+}
 
 public static void main(String[] args) {
 	PhoneInfo a=new PhoneInfo();
