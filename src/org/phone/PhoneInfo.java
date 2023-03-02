7c2285affc8b68b9e349ce3217b1ff15f21c9dd1
karthikvicky22 <karthiksharon003@gmail.com>
2023-03-02 12:13:05 +0530

    code added

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -8,23 +8,13 @@ System.out.println("karthi");
 private void phoneNumb() {
 System.out.println("9629087326");
 }
-private void camera() {
-System.out.println("16mhp");
-}
-private void storage() {
-System.out.println("32gb");
-}
-private void osName() {
-System.out.println("android");
-}
+
+
 public static void main(String[] args) {
 	PhoneInfo a=new PhoneInfo();
 	
 a.phoneName();
 a.phoneNumb();
-a.camera();
-a.storage();
-a.osName();
 			
 }
 }
